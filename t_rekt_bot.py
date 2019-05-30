7e1f34be12f40ef633914b855b763839b721e1a9
Lucas Keller <kellerjlucas@gmail.com>
2019-05-30 13:51:31 -0500

    Updated Readme, refactored code for PEP8 standards

--- t_rekt_bot.py
+++ t_rekt_bot.py
@@ -15,45 +15,45 @@ def bot_login():
 #Looks for certain keywords
 def run_bot_search(r: Reddit):
     #Declare the words we are searching for
-    SearchStrings = ['helser', 'hot']
-    CommentCounter = 0
+    search_strings = ['helser', 'hot']
+    comment_counter = 0
 
-    Word1, Word2 = SearchStrings
+    word1, word2 = search_strings
 
     for comment in r.subreddit('iastate').comments(limit=1000):
-        if Word1 in comment.body.lower() and Word2 in comment.body.lower():
-            CommentCounter += 1
+        if word1 in comment.body.lower() and word2 in comment.body.lower():
+            comment_counter += 1
             print(f'I found the comment: {comment.body}. It has the author {comment.author.name}')
             #comment.reply("I heard you mentioned geese: lets page /u/Tyrannosaurus-Rekt.")
-    print(f'I found {CommentCounter} comments containing {SearchStrings}.')
+    print(f'I found {comment_counter} comments containing {search_strings}.')
 
 def run_user_search(r: Reddit):
     #Define a search user, create a list to track all comments
-    SearchUser = config.HiddenUser
+    search_user = config.HiddenUser
 
     #Track comments replied to
-    CommentsRepliedTo = []
+    comments_replied_to = []
 
     #Track time of newest submission and submission in stream
-    NewMessageTimeUTC = 0
-    CurrentCommentTimeUTC = 0
+    new_message_time_utc = 0
+    current_comment_time_utc = 0
 
     #looks at new comments made by user to a limited amount
-    for comment in r.redditor(SearchUser).comments.new(limit=1):
-        CommentsRepliedTo.append(comment.id)
-        NewMessageTimeUTC = comment.created_utc
+    for comment in r.redditor(search_user).comments.new(limit=1):
+        comments_replied_to.append(comment.id)
+        new_message_time_utc = comment.created_utc
         print(f'The most recent comment was {comment.body!r}')
 
 
     #Streams new comments in
-    for comment in r.redditor(SearchUser).stream.comments():
-        CurrentCommentTimeUTC = comment.created_utc
+    for comment in r.redditor(search_user).stream.comments():
+        current_comment_time_utc = comment.created_utc
         #If new comment is found, reply and update
-        if(CurrentCommentTimeUTC > NewMessageTimeUTC):
+        if(current_comment_time_utc > new_message_time_utc):
             print(f'I found a new comment {comment.body!r}')
             comment.reply("Hey Eric! Get off reddit and do something useful. You're probably boasting about your elo right now. *Love you baby*")
-            CommentsRepliedTo.append(comment.id)
-            NewMessageTimeUTC = CurrentCommentTimeUTC
+            comments_replied_to.append(comment.id)
+            new_message_time_utc = current_comment_time_utc
 
 #Looks for certain user and goes through their comments
 r = bot_login()
